569fa8a9facc6db4a7d88d20b3f7007c95479e3a
PENG WANG <wangpeng@se.cuhk.edu.hk>
2021-05-27 17:38:05 +0800

    Add files via upload

--- recovery-efficiency-accuracy/SC.m
+++ recovery-efficiency-accuracy/SC.m
@@ -4,14 +4,14 @@ function e = SC(A, n, K)
 %%%% Algorithm 1 of "Consistency of Spectral Clustering in SBMs" (Lei & Rinaldo, 2015) %%%% 
 
     % fprintf(' ******************** Spectral Clustering for Exact Recovery *************************** \n')
-    %% computing K leading eigenvectors of graph Laplician
+    %% compute K leading eigenvectors of graph Laplician
     [U, D] = eigs(A, K);   
     ev = diag(D);  
     [~,I] = sort(ev,'descend');
     ev = ev(I);        
     U = U(:,I); U = normr(U);
     
-    %% Applying K-means to do clustering
+    %% apply K-means to do clustering
     e = kmeans(U, K, 'replicates', 50);  
 %     Q = zeros(n,K);
 %     
